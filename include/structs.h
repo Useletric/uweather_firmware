5ccff14fd60dc531c695db561dd73defe5a0f096
isbrun <issacar.brunow@el.com.br>
2023-11-29 10:45:19 -0300

    feat biblioteca dos structs

---
+++ include/structs.h
@@ -0,0 +1,42 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+    /* Importação do framework Arduino */
+    #include <Arduino.h>
+
+        struct anemometro{ /* Struct referente aos dados da rede e do A7670SA */
+            // --- Constantes ---
+            const float pi = 3.14159265;     //Número de pi
+            int period = 5000;               //Tempo de medida(miliseconds)
+            int delaytime = 2000;            //Invervalo entre as amostras (miliseconds)
+            int radius = 147;                //Raio do anemometro(mm)
+
+            // --- Variáveis Globais ---
+            unsigned int Sample  = 0;        //Armazena o número de amostras
+            unsigned int counter = 0;        //Contador para o sensor
+            unsigned int RPM = 0;            //Rotações por minuto
+            float speedwind = 0;             //Velocidade do vento (m/s)
+            float windspeed = 0;             //Velocidade do vento (km/h)
+        };
+
+        struct bme280{
+            float temp = 0;
+            float umi = 0;
+            float pressure =  0;
+            float altitude = 0;
+        };
+
+        struct tensaoPainel{
+
+        };
+
+        struct tensaoBateria{
+
+        };
+
+        extern struct anemometro    struct_anemometro;
+        extern struct bme280        struct_bme280;
+        extern struct tensaoPainel  struct_tensaoPainelSolar;
+        extern struct tensaoBateria struct_tensaoBateriaInterna;
+
+#endif
