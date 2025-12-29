174f42856cba6f4533532f6b85776256c5d839ac
issacarbrun <issacarbrunow@gmail.com>
2025-12-29 08:39:28 -0300

    retorno de estado anterior

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ void setup()
 
   pinMode(ANEMO_PIN, INPUT);
   pinMode(RAIN_SENSOR_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(RAIN_SENSOR_PIN), rainTrigger, FALLING  );
+  attachInterrupt(digitalPinToInterrupt(RAIN_SENSOR_PIN), rainTrigger, CHANGE  );
   getID();
   init_sd();
   loadConfiguration("/config.txt");
